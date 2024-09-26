785a19a2559105eb3a4ddfa63bdd469a8b05daf0
Yury Stolarz <yurysscareer@gmail.com>
2024-09-26 19:30:57 -0300

    docs: add endpoints documentation with swagger

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
 import setupRoutes from './routes';
+import swaggerDocument from '../docs/swagger.json';
 
 const app = express();
 
@@ -9,6 +11,7 @@ app.use(morgan('combined'));
 app.use(helmet());
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 setupRoutes(app);
 
 export default app;
