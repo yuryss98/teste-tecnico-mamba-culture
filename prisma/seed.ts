96f70230f09d1ec913e945479285133ca1a4b458
Yury Stolarz <yurysscareer@gmail.com>
2024-09-25 21:27:31 -0300

    chore: create seed

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+async function main() {
+  await prisma.categories.createMany({
+    data: [
+      {
+        name: 'Campanhas de Marketing',
+      },
+      {
+        name: 'Campanhas de Conscientização',
+      },
+      {
+        name: 'Campanhas de Vendas',
+      },
+    ]
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
